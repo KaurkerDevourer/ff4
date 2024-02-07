9ea296e8c88a21136f38992c139b4e6789fb4053
Stepan Styopkin <smstyopkin@edu.hse.ru>
2024-02-07 10:30:00 +0000

    much better buchberger with queue

--- lib/algo/buchberger.cpp
+++ lib/algo/buchberger.cpp
@@ -1,23 +1,29 @@
 #include "buchberger.h"
+#include <queue>
 
 namespace NAlgo {
     namespace Buchberger {
 
         void DoProcess(NUtils::TPolynomials& F) {
-            bool ready = false;
-            while(!ready) {
-                ready = true;
-                for (size_t i = 0; i < F.size(); i++) {
-                    for (size_t j = i + 1; j < F.size(); j++) {
-                        const NUtils::Monomial& gi = F[i].GetHeadMonomial();
-                        const NUtils::Monomial& gj = F[j].GetHeadMonomial();
-                        NUtils::Monomial glcm = lcm(gi, gj);
-                        NUtils::Polynomial S = F[i] * (glcm/gi) - F[j] * (glcm/gj);
-                        if (!S.ReduceBy(F)) {
-                            ready = false;
-                            F.push_back(S);
-                        }
+            std::queue<std::pair<size_t, size_t> > pairs_to_check;
+            for (size_t i = 0; i < F.size(); i++) {
+                for (size_t j = i + 1; j < F.size(); j++) {
+                    pairs_to_check.push({i, j});
+                }
+            }
+            while(pairs_to_check.size()) {
+                const NUtils::Polynomial& fi = F[pairs_to_check.front().first];
+                const NUtils::Polynomial& fj = F[pairs_to_check.front().second];
+                pairs_to_check.pop();
+                const NUtils::Monomial& gi = fi.GetHeadMonomial();
+                const NUtils::Monomial& gj = fj.GetHeadMonomial();
+                NUtils::Monomial glcm = lcm(gi, gj);
+                NUtils::Polynomial S = fi * (glcm/gi) - fj * (glcm/gj);
+                if (!S.ReduceBy(F)) {
+                    for (size_t i = 0; i < F.size(); i++) {
+                        pairs_to_check.push({i, F.size()});
                     }
+                    F.push_back(S);
                 }
             }
         }
