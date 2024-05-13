8f84f79abc3a673c606765b3f8efdec48b718601
Stepan Styopkin <smstyopkin@edu.hse.ru>
2024-05-13 16:16:58 +0300

    try no copy

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -9,9 +9,9 @@ int main() {
         std::cout << "DEBUG MODE" << std::endl;
     #endif
     // benchmark_cyclic4_rational();
-    // benchmark_cyclic4_prime_field();
-    // benchmark_katsura4();
-    // benchmark_sym3_3();
+    benchmark_cyclic4_prime_field();
+    benchmark_katsura4();
+    benchmark_sym3_3();
     benchmark_cyclic5();
     benchmark_cyclic6();
     benchmark_cyclic7();
